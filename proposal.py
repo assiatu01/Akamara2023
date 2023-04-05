3b28ca2eac25ccfa9c2a46fd2ba24c47bbcb7df6
assiatu01 <129916190+assiatu01@users.noreply.github.com>
2023-04-04 21:45:35 -0400

    Add files via upload

---
+++ proposal.py
@@ -0,0 +1,7 @@
+
+# we are thinking about doing our project kindo of like a game show. "Are You Smarter than a fifth grader" 
+it will be sceince based since we are all science majors. 
+
+
+
+# I will be working with two partners, Seykon Toee and Rosselinne Nunez. 
